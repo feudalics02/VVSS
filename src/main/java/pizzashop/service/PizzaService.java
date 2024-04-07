0bf91dfe6e779c48d38fca1016f1c1378d169919
alex <alex_isac72@yahoo.com>
2024-04-07 11:14:19 +0300

    lab03

--- src/main/java/pizzashop/service/PizzaService.java
+++ src/main/java/pizzashop/service/PizzaService.java
@@ -15,37 +15,47 @@ public class PizzaService {
     private MenuRepository menuRepo;
     private PaymentRepository payRepo;
 
-    public PizzaService(MenuRepository menuRepo, PaymentRepository payRepo){
-        this.menuRepo=menuRepo;
-        this.payRepo=payRepo;
+    public PizzaService(MenuRepository menuRepo, PaymentRepository payRepo) {
+        this.menuRepo = menuRepo;
+        this.payRepo = payRepo;
     }
 
-    public List<PizzaItem> getMenuData() throws OrderException {return menuRepo.getMenu();}
+    public List<PizzaItem> getMenuData() throws OrderException {
+        return menuRepo.getMenu();
+    }
 
-    public List<Payment> getPayments(){return payRepo.getAll(); }
+    public List<Payment> getPayments() {
+        return payRepo.getAll();
+    }
 
     public void addPayment(Object table, PaymentType type, double amount) throws PaymentException {
-        if(!(table instanceof Integer)){
+        if (!(table instanceof Integer)) {
             throw new PaymentException("Invalid table type");
         }
 
-        if((int)table<1 || (int)table > 8){
+        if ((int) table < 1 || (int) table > 8) {
             throw new PaymentException("Invalid table");
         }
-        if(amount<1 || amount>100000){
+        if (amount < 1 || amount > 100000) {
             throw new PaymentException("Invalid amount");
         }
-        Payment payment= new Payment((int)table, type, amount);
+        Payment payment = new Payment((int) table, type, amount);
         payRepo.add(payment);
     }
 
-    public double getTotalAmount(PaymentType type){
-        double total=0.0f;
-        List<Payment> l=getPayments();
-        if ((l==null) ||(l.size()==0)) return total;
-        for (Payment p:l){
-            if (p.getType().equals(type))
-                total+=p.getAmount();
+    public double getTotalAmount(PaymentType type) {
+        double total = 0.0f;
+        List<Payment> l = getPayments();
+        if (l == null) {
+            return total;
+        }
+        if (l.size() == 0) {
+            return total;
+        }
+        for (Payment p : l) {
+            if (p.getType().equals(type)) {
+                total += p.getAmount();
+            }
         }
         return total;
     }
